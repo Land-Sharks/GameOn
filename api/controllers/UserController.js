36a9861964da1ae52b3ca40a43b722d34f2d442e
derickfan <fanderick9999@gmail.com>
2020-10-27 14:26:23 -0400

    add minor login feature

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -14,10 +14,29 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
 
     try {
+        console.log(req.body)
         const user = await User.create(req.body);
         res.status(201).send(user);
     } catch {
         res.status(400).send("Error");
+    }
+
+});
+
+router.post('/login', async (req, res) => {
+
+    try {
+        const user = await User.findOne({
+            where: {
+                username: req.body.username,
+                password: req.body.password
+            }
+        });
+        console.log(user);
+        
+        res.status(200).send(user);
+    } catch {
+        res.status(404).send(user);
     }
 
 });
