e6de58b0402a415d4177288ccc026aca9b906367
derickfan <fanderick9999@gmail.com>
2020-11-05 17:05:00 -0500

    format doc

--- api/models/Game.js
+++ api/models/Game.js
@@ -1,28 +1,31 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-    class Game extends Model {}
+	class Game extends Model {}
 
-    Game.init({
-        name: {
-            type: DataTypes.STRING,
-            unique: true,
-        },
-    }, {
-        sequelize,
-        // Disables the createdAt & updatedAt data that sequelize auto generates 
-        timestamps: false,
-        modelName: 'game'
-    });
+	Game.init(
+		{
+			name: {
+				type: DataTypes.STRING,
+				unique: true,
+			},
+		},
+		{
+			sequelize,
+			// Disables the createdAt & updatedAt data that sequelize auto generates
+			timestamps: false,
+			modelName: "game",
+		}
+	);
 
-    Game.associate = (models) => {
+	Game.associate = (models) => {
+		// Creates a many-to-many relation between Game and Genre
+		models.Game.belongsToMany(models.Genre, {
+			through: "GameGenres",
+			timestamps: false,
+		});
+	};
 
-        // Creates a many-to-many relation between Game and Genre
-        models.Game.belongsToMany(models.Genre, { through: "GameGenres", timestamps: false });
-
-    }
-
-    return Game;
-
-}
+	return Game;
+};
--- api/models/Genre.js
+++ api/models/Genre.js
@@ -1,28 +1,31 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-    class Genre extends Model {}
+	class Genre extends Model {}
 
-    Genre.init({
-        name: {
-            type: DataTypes.STRING,
-            unique: true,
-        },
-    }, {
-        sequelize,
-        // Disables the createdAt & updatedAt data that sequelize auto generates 
-        timestamps: false,
-        modelName: 'genre'
-    })
+	Genre.init(
+		{
+			name: {
+				type: DataTypes.STRING,
+				unique: true,
+			},
+		},
+		{
+			sequelize,
+			// Disables the createdAt & updatedAt data that sequelize auto generates
+			timestamps: false,
+			modelName: "genre",
+		}
+	);
 
-    Genre.associate = (models) => {
+	Genre.associate = (models) => {
+		// Creates a many-to-many relation between Genre and Game
+		models.Genre.belongsToMany(models.Game, {
+			through: "GameGenres",
+			timestamps: false,
+		});
+	};
 
-        // Creates a many-to-many relation between Genre and Game
-        models.Genre.belongsToMany(models.Game, { through: "GameGenres", timestamps: false });
-
-    }
-
-    return Genre;
-
-}
+	return Genre;
+};
