afc2a450bc6360f3acada076890032ef9e099020
derickfan <fanderick9999@gmail.com>
2020-11-13 12:10:11 -0500

    add follow and user's followed games endpoint

--- api/controllers/GameController.js
+++ api/controllers/GameController.js
@@ -3,38 +3,58 @@ const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
 const db = require("../models");
-const { Game, Genre } = db;
+const { Game, Genre, User } = db;
 
 // Returns a list of all games
 router.get("/", async (req, res) => {
 	const genre = req.query.genre;
+	console.log(req.query);
 	try {
-		const games = genre ? await Game.findAll({
-			attributes: ["name"],
-			include: {
+
+		const obj = {
+			order: [["name", "ASC"]],
+			include: [{
 				model: Genre,
-				where: {
-					name: genre,
-                },
-                // Hides the genre name as its redundant
-				attributes: [],
-				// Hides the associative table GameGenres from results
 				through: { attributes: [] },
-			},
-		})
-		: await Game.findAll({
-			attributes: ['name'], 
-			// include: {
-			// 	model: Genre,
-			// 	attributes: ['name'],
-			// 	through: { attributes: [] },
-			// }
-		});
+			}]
+		}
+
+		if (genre) {
+			obj['include'][0]['where'] = { name: genre }
+		}
+
+		const games = await Game.findAll(obj);
 		res.status(200).json(games);
 	} catch {
 		res.status(404).send("Not Found");
 	}
 });
+
+router.get("/:username", async (req, res) => {
+	try {
+		const username = req.params.username;
+		const games = await Game.findAll({
+			limit: 30, 
+			order: [["name", "ASC"]],
+			include: [{
+				model: Genre, 
+				through: { attribute: [] },
+			}, {
+				model: User,
+				where: {
+					username: username,
+				},
+				attributes: ['username'], 
+				through: { attributes: [] },
+				required: false
+			}]
+		})
+
+		res.status(200).json(games);
+	} catch {
+		res.status(404).send("Not Found");
+	}
+})
 
 // Add a game to the database
 router.post("/", async (req, res) => {
@@ -61,20 +81,52 @@ router.delete("/", async (req, res) => {
 	}
 });
 
-
 router.get("/:game", async (req, res) => {
 	try {
 		const game = req.params.game;
-		console.log(game);
 		const result = await Game.findOne({
 			where: { name: game },
 			include: {
-				model: Genre
-			}
-		})
+				model: Genre,
+			},
+		});
 		res.status(200).send(result);
 	} catch {
 		res.status(400).send("Error");
+	}
+});
+
+router.post("/follow", async (req, res) => {
+	try {
+		const gameId = req.body.gameId;
+		const username = req.body.username;
+
+		const association = await User.findOne({
+			where: { username: username }, 
+			include: {
+				model: Game,
+				where: {
+					id: gameId
+				}
+			}
+		});
+
+		const game = await Game.findOne({
+			where: { id: gameId },
+		});
+		const user = await User.findOne({
+			where: { username: username },
+		});
+
+		if (association === null) {
+			await user.addGame(game);
+		} else {
+			await user.removeGame(game);
+		}
+
+		res.status(200).json({ message: "Operation Successful" });
+	} catch {
+		res.status(405).json({ message: "Cannot complete operation" });
 	}
 });
 
@@ -87,11 +139,11 @@ router.post("/fill", async (req, res) => {
 		if (response.ok) {
 			const data = await response.json();
 			const games = await data.results.map((e) => {
-				return { name: e.name, genres: e.genres.map(e => e.name) };
+				return { name: e.name, genres: e.genres.map((e) => e.name) };
 			});
 			games.forEach(async (e) => {
 				await addGames(e.name, e.genres);
-			})
+			});
 			listOfGames.push(games);
 		} else {
 			console.log(response);
@@ -105,14 +157,16 @@ const addGames = async (name, genres) => {
 	const game = await Game.create({
 		name: name,
 	});
-	const g = await Promise.all(genres.forEach(async (e) => {
-		const [ genre, created ] = await Genre.findOrCreate({
-			where: { name: e }
-		});
-		await game.addGenre(genre);
-	}));
-	
+	const g = await Promise.all(
+		genres.forEach(async (e) => {
+			const [genre, created] = await Genre.findOrCreate({
+				where: { name: e },
+			});
+			await game.addGenre(genre);
+		})
+	);
+
 	return game;
-} 
+};
 
 module.exports = router;
