4a053364382e14921b0f3abc482796edca59d5fd
derickfan <fanderick9999@gmail.com>
2020-11-20 02:30:32 -0500

    add class logo

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,46 +1,54 @@
-import React, { Fragment } from 'react';
-import { useHistory } from 'react-router-dom';
-import './NavBar.css';
+import React, { Fragment } from "react";
+import { useHistory } from "react-router-dom";
+import "./NavBar.css";
 
-import auth from '../../services/auth';
+import auth from "../../services/auth";
 
 const NavBar = (props) => {
+	const history = useHistory();
 
-    const history = useHistory();
+	const logout = async () => {
+		const res = await auth.signout();
+		history.go(0);
+	};
 
-    const logout = async () => {
-        const res = await auth.signout();
-        history.go(0);
-    }
+	const goToGames = () => {
+		history.push("games");
+	};
 
-    const goToGames = () => {
-        history.push("games");
-    }
+	const goToProfile = () => {
+		history.push("profile");
+	};
 
-    const goToProfile = () => {
-        history.push("profile")
-    }
+	return (
+		<nav>
+			<div class="head">
+				<img className="logo" src="/assets/images/logo.svg" alt="" />
+				<a href="/">GameOn</a>
+			</div>
+			{auth.isAuthenticated ? (
+				<Fragment>
+					<input type="text" />
+					<input
+						type="button"
+						onClick={goToProfile}
+						value="Profile"
+					/>
+					<input type="button" onClick={goToGames} value="Games" />
 
-    return <nav>
-        <div class="head">
-       <img src = "/assets/images/logo.svg" alt=""/>
-       <a href="/">GameOn</a>
-       </div>
-        {
-            auth.isAuthenticated 
-            ? <Fragment>
-                    <input type="text" />
-                    <input type="button" onClick={goToProfile} value="Profile" />
-                    <input type="button" onClick={goToGames} value="Games" />
-                    
-                    <input type="button" onClick={logout} value="Logout" />
-                </Fragment>
-                : <Fragment>
-                    <input type="button" onClick={props.displayLoginForm} value="Login" />
-                </Fragment>
-        }
-    </nav>
-
-}
+					<input type="button" onClick={logout} value="Logout" />
+				</Fragment>
+			) : (
+				<Fragment>
+					<input
+						type="button"
+						onClick={props.displayLoginForm}
+						value="Login"
+					/>
+				</Fragment>
+			)}
+		</nav>
+	);
+};
 
 export default NavBar;
