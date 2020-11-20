40f1062bedb3e1db448cebb8615f5a5d9adfddc0
derickfan <fanderick9999@gmail.com>
2020-11-20 02:25:32 -0500

    add post button

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "../../components/NavBar/NavBar";
 import PostContainer from "../../components/PostContainer/PostContainer";
+import PostForm from "../../components/PostForm/PostForm";
 import "./HomePage.css";
 
 const HomePage = (props) => {
 
+	const [ posting, setPosting ] = useState(false);
+
+	const toggleForm = () => {
+		setPosting(!posting);
+	}
+
 	return (
 		<div className="home-page">
-			<NavBar />
+			<NavBar toggleForm={toggleForm} />
+			{
+				posting ? <PostForm toggleForm={toggleForm}/> : <div />
+			}
 			<PostContainer />
 		</div>
 	);
