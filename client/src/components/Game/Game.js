e58a94e9ff116791d525b5a3be5c839fbe6360a9
derickfan <fanderick9999@gmail.com>
2020-11-20 02:22:34 -0500

    display game img

--- client/src/components/Game/Game.js
+++ client/src/components/Game/Game.js
@@ -1,5 +1,4 @@
-import { use } from 'passport';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Game.css';
 
 import auth from '../../services/auth';
@@ -7,6 +6,20 @@ import auth from '../../services/auth';
 const Game = (props) => {
 
     const [ isFollowing, setIsFollowing ] = useState(props.following);
+
+    const [ imageUrl , setImageUrl ] = useState()
+
+    const getImageUrl = () => {
+        const baseUrl = "https://static-cdn.jtvnw.net/ttv-boxart/";
+        const gameUrl = props.name.replace(" ", "%20");
+        const dimenUrl = "-276x368.jpg";
+        const url = baseUrl + gameUrl + dimenUrl;
+        return url;
+    }
+
+    useEffect(() => {
+        setImageUrl(getImageUrl());
+    });
 
     const followGame = async () => {
 
@@ -37,7 +50,7 @@ const Game = (props) => {
                     <p>{g.name}</p>
                 ))}
             </div>
-            
+            <img className="game-image" src={imageUrl} /> 
             <input className={isFollowing ? 'red' : 'green'} type="button" onClick={followGame} value={isFollowing ? 'unfollow' : 'follow'}/>
         </div>
     )
